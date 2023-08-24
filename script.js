d66d426d4387aae006d6914f9c3890a6749aa12f
Jitender Kumar <111197506+Jitender72658@users.noreply.github.com>
2023-08-24 15:50:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ const inputs = document.querySelectorAll('.controls input');
 
     inputs.forEach(input => input.addEventListener('change', handleUpdate));
     inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+const video = document.getElementById('myVideo');
+const playButton = document.getElementById('playButton');
+const pauseButton = document.getElementById('pauseButton');
+
+// Add click event listeners to the play and pause buttons
+playButton.addEventListener('click', () => {
+  video.play();
+});
+
+pauseButton.addEventListener('click', () => {
+  video.pause();
+});
